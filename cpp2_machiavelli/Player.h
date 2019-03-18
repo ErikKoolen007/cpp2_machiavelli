73c7a80bea5e2ceb51b45de7b8524173aac75e00
Erik Koolen <erik-koolen@online.nl>
2019-03-18 20:26:06 +0100

    added filters, changed player

--- cpp2_machiavelli/Player.h
+++ cpp2_machiavelli/Player.h
@@ -1,26 +1,20 @@
-//
-//  Player.hpp
-//  socketexample
-//
-//  Created by Bob Polis on 23-11-15.
-//  Copyright © 2015 Avans Hogeschool, 's-Hertogenbosch. All rights reserved.
-//
-
-#ifndef Player_hpp
-#define Player_hpp
+#pragma once
 
 #include <string>
+#include <utility>
 
 class Player {
 public:
-    Player() {}
-    Player(const std::string& name) : name {name} {}
+    Player() = default;
+	Player(std::string name) : name_ {std::move(name)}
+	{
+	}
 
-    std::string get_name() const { return name; }
-    void set_name(const std::string& new_name) { name = new_name; }
+    std::string get_name() const { return name_; }
+    void set_name(const std::string& new_name) { name_ = new_name; }
 
 private:
-    std::string name;
+    std::string name_;
+	int points_ = 0;
+	int coins_ = 0;
 };
-
-#endif /* Player_hpp */
